2cc418644c999582c34d4dd3be8701dbba7a79e8
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2026-05-28 15:14:59 +0900

    ios用のサブプロジェクトを追加

---
+++ cycle_computer_iosAPP/cycle_computer_iosAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cycle_computer_iosAPP
+//
+//  Created by ricky_sensei on 2026/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cycle_computer_iosAPP/cycle_computer_iosAPP/cycle_computer_iosAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  cycle_computer_iosAPPApp.swift
+//  cycle_computer_iosAPP
+//
+//  Created by ricky_sensei on 2026/05/28.
+//
+
+import SwiftUI
+
+@main
+struct cycle_computer_iosAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
